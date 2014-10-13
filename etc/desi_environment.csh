cfdb489e49a4b7874a7c1c28a7654478bd1cd6e4
Stephen Bailey <stephen.code@gmail.com>
2014-10-13 03:45:11 +0000

    update desi_environment for carver; update modules for desimodel and python prerequisites

--- etc/desi_environment.csh
+++ etc/desi_environment.csh
@@ -1,22 +1,43 @@
 #
 # This code is meant to be sourced, not executed.
+# It currently only supports NERSC hosts.
 #
 # $Id$
 #
 if ( `basename ${SHELL}` == "csh" || `basename ${SHELL}` == "tcsh" ) then
     source /project/projectdirs/cmb/modules/hpcports_NERSC.csh
-    if ( "${NERSC_HOST}" == "carver" || "${NERSC_HOST}" == "hopper" )  then
+    if ( "${NERSC_HOST}" == "edison" ) then
+        hpcports shared_gnu
+    endif
+    if ( "${NERSC_HOST}" == "carver" )  then
+        if ( "${CHOS}" != "sl6carver" ) then
+            echo "ERROR: The DESI environment requires Scientific Linux 6 (SL6) on carver."
+            echo "Please switch to SL6 first by creating a .chos-carver file in your home"
+            echo "directory with contents \"sl6carver\" and then login again;"
+            echo "or run \"export CHOS=sl6carver; chos bash -l\" to temporarily switch to SL6."
+            echo
+            echo "For more details about CHOS and SL5 vs SL6 on carver, see"
+            echo "https://www.nersc.gov/users/computational-systems/carver/user-environment/"
+            echo
+            echo "After switching to SL6, source desi_environment.sh again."
+            return
+        endif 
+        hpcports gnu
+    endif
+    if ( "${NERSC_HOST}" == "hopper" )  then
         hpcports gnu
     endif
     if ( "${NERSC_HOST}" == "datatran" || "${NERSC_HOST}" == "scigate" ) then
         hpcports
     endif
-    if ( "${NERSC_HOST}" == "edison" ) then
-        hpcports shared_gnu
-    endif
     if ( -d /project/projectdirs/desi/software/modules/${NERSC_HOST} ) then
         module use /project/projectdirs/desi/software/modules/${NERSC_HOST}
-        module load desiModules
+        
+        if ( $# > 0 ) then
+            module load desiModules/$1
+        else
+            module load desiModules
+        endif
     else
         echo "Could not find DESI modules for ${NERSC_HOST}!"
     endif
--- etc/desi_environment.sh
+++ etc/desi_environment.sh
@@ -1,22 +1,43 @@
 #
 # This code is meant to be sourced, not executed.
+# It currently only supports NERSC hosts.
 #
 # $Id$
 #
 if [[ $(basename ${SHELL}) == "bash" || $(basename ${SHELL}) == "sh" ]]; then
     source /project/projectdirs/cmb/modules/hpcports_NERSC.sh
-    if [[ "${NERSC_HOST}" == "carver" || "${NERSC_HOST}" == "hopper" ]]; then
+    if [[ "${NERSC_HOST}" == "edison" ]]; then
+        hpcports shared_gnu
+    fi
+    if [[ "${NERSC_HOST}" == "carver" ]]; then
+        #- On carver, check if we are Scientific Linux 5, 6, or default
+        if [[ "${CHOS}" != "sl6carver" ]]; then
+            echo "ERROR: The DESI environment requires Scientific Linux 6 (SL6) on carver."
+            echo "Please switch to SL6 first by creating a .chos-carver file in your home"
+            echo "directory with contents \"sl6carver\" and then login again;"
+            echo "or run \"export CHOS=sl6carver; chos bash -l\" to temporarily switch to SL6."
+            echo
+            echo "For more details about CHOS and SL5 vs SL6 on carver, see"
+            echo "https://www.nersc.gov/users/computational-systems/carver/user-environment/"
+            echo
+            echo "After switching to SL6, source desi_environment.sh again."
+            return
+        fi
+        hpcports gnu
+    fi
+    if [[ "${NERSC_HOST}" == "hopper" ]]; then
         hpcports gnu
     fi
     if [[ "${NERSC_HOST}" == "datatran" || "${NERSC_HOST}" == "scigate" ]]; then
         hpcports
     fi
-    if [[ "${NERSC_HOST}" == "edison" ]]; then
-        hpcports shared_gnu
-    fi
     if [[ -d /project/projectdirs/desi/software/modules/${NERSC_HOST} ]]; then
         module use /project/projectdirs/desi/software/modules/${NERSC_HOST}
-        module load desiModules
+        if [[ $# > 0 ]]; then
+            module load desiModules/$1
+        else
+            module load desiModules
+        fi
     else
         echo "Could not find DESI modules for ${NERSC_HOST}!"
     fi
