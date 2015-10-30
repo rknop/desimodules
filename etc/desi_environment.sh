b02e1d94bea5f37af95e157a75dbfb3d58dce669
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-10-30 17:58:35 -0400

    add requests and remove references to carver

--- etc/desi_environment.sh
+++ etc/desi_environment.sh
@@ -12,22 +12,6 @@ if [[ $(basename ${SHELL}) == "bash" || $(basename ${SHELL}) == "sh" ]]; then
     if [[ "${NERSC_HOST}" == "hopper" ]]; then
         hpcports shared_gnu
     fi
-    if [[ "${NERSC_HOST}" == "carver" ]]; then
-        #- On carver, check if we are Scientific Linux 5, 6, or default
-        if [[ "${CHOS}" != "sl6carver" ]]; then
-            echo "ERROR: The DESI environment requires Scientific Linux 6 (SL6) on carver."
-            echo "Please switch to SL6 first by creating a .chos-carver file in your home"
-            echo "directory with contents \"sl6carver\" and then login again;"
-            echo "or run \"export CHOS=sl6carver; chos bash -l\" to temporarily switch to SL6."
-            echo
-            echo "For more details about CHOS and SL5 vs SL6 on carver, see"
-            echo "https://www.nersc.gov/users/computational-systems/carver/user-environment/"
-            echo
-            echo "After switching to SL6, source desi_environment.sh again."
-            return
-        fi
-        hpcports gnu
-    fi
     if [[ "${NERSC_HOST}" == "datatran" || "${NERSC_HOST}" == "scigate" ]]; then
         hpcports
     fi
